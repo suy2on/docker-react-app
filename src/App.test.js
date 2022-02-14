5ca6b61ef3f18da209128cab61f7a0e41ba7e8d3
suy2on <tnwjdrms@naver.com>
2022-02-14 23:43:08 +0900

    # feat : test 수정

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(i);
   expect(linkElement).toBeInTheDocument();
 });
 
